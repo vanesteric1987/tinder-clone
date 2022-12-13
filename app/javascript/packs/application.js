03baa28d95cbae2f27484255e45506e7038b3a43
Stephanie Ramiaramanana <vanesteric1987@gmail.com>
2022-12-13 15:24:31 +0100

    suite

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,14 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
+// import Rails from "@rails/ujs"
+require("@rails/ujs").start()
+// import Turbolinks from "turbolinks"
+require("turbolinks").start()
+// import * as ActiveStorage from "@rails/activestorage"
+require("@rails/activestorage").start()
+// Rails.start()
+// Turbolinks.start()
+// ActiveStorage.start()
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+require("bootstrap").start()
